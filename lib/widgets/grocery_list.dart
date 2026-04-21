0c1d57177c2adf154d884233c68410db76fb681d
dimon-ton <saichon2namoom@gmail.com>
2026-04-21 07:17:44 +0700

    I've finished the section in EP 212. Adding a 'New Item' Screen

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GloceryList extends StatelessWidget {
+class GloceryList extends StatefulWidget {
   const GloceryList({super.key});
+
+  @override
+  State<GloceryList> createState() => _GloceryListState();
+}
+
+class _GloceryListState extends State<GloceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Grocceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
